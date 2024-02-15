d14ccb96a2025e5d8df5a4ea69ea3b606bb32b29
FaisalAlmadafei <113466869+FaisalAlmadafei@users.noreply.github.com>
2024-02-15 18:23:10 +0300

    Completed technical member home page

--- my-app/src/SharedComponents/Service.js
+++ my-app/src/SharedComponents/Service.js
@@ -1,24 +1,31 @@
 import React from "react";
 import "../SharedCSS/Service.css";
 
-function Service({serviceName, Icon}) {
+function Service({ serviceName, notification, Icon }) {
+  let reportsNotifications = 3 ; 
+  let requsetsNotifications = 2;
+  let Notifications ; 
+  if (notification === "true") {
+    Notifications = serviceName === "Reports" ? reportsNotifications : requsetsNotifications; 
+  }
   return (
-   <div >
-
-    <div className="card">
-     <Icon className="react-icons" />
-     <br />
-     <h3 className="service-name">{serviceName}</h3>
-
+    <div>
+      {notification == "true" ? (
+        <div className="card">
+        <div className="notification">{Notifications}</div>
+        <Icon className="react-icons" />
+        <br />
+        <h3 className="service-name">{serviceName}</h3>
+   
+       </div>
+      ) : (
+        <div className="card">
+          <Icon className="react-icons" />
+          <br />
+          <h3 className="service-name">{serviceName}</h3>
+        </div>
+      )}
     </div>
-
-
-
-
-
-   </div>
-
-
   );
 }
 
