4ffe0e895dd4c19e750088ffc71df8bd018efeaf
FaisalAlmadafei <113466869+FaisalAlmadafei@users.noreply.github.com>
2024-02-16 14:03:19 +0300

    Fixed css for mobile amd changes icons in supervisor page

--- my-app/src/TechnicalSupervisor/TsvHomePage.js
+++ my-app/src/TechnicalSupervisor/TsvHomePage.js
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { GrDocumentText } from "react-icons/gr";
-import { MdOutlineAddTask } from "react-icons/md";
 import { TbDeviceDesktopSearch } from "react-icons/tb";
 import { TbDeviceDesktopCheck } from "react-icons/tb";
 import { TbDeviceDesktopCog } from "react-icons/tb";
 import { MdHistoryEdu } from "react-icons/md";
+import { RiTeamLine } from "react-icons/ri";
 import Service from "../SharedComponents/Service";
 import "../SharedCSS/HomePage.css";
 
-export default function TsvHomePage() {
+function TsvHomePage() {
   const navigate = useNavigate();
   let services = [
     { serviceName: "Reports", icon: GrDocumentText, notification: "true" ,pageLink: "/LabsPage" },
     { serviceName: "Requests", icon: MdHistoryEdu, notification: "true",pageLink: "/LabsPage" },
-    { serviceName: "Assigned Tasks", icon: MdOutlineAddTask, notification: "true",pageLink: "/LabsPage" },
+    { serviceName: "Team Progress", icon: RiTeamLine, notification: "false" ,pageLink: "/LabsPage" },
     {
       serviceName: "Search for a device",
       icon: TbDeviceDesktopSearch,
@@ -33,28 +33,31 @@ export default function TsvHomePage() {
   const filteredServices = services.filter(service => service.serviceName.toLowerCase().includes(search.toLowerCase()));
 
   return (
-<div>
+    <div>
     
-    <div className='navBar'>
-      <input type="text" placeholder="Serach for a service" className="search-bar" onChange={(e) => setSearch(e.target.value)}/>
+      <div className='navBar'>
+        <input type="text" placeholder="Serach for a service" className="search-bar" onChange={(e) => setSearch(e.target.value)}/>
 
+        </div>
+       <div className="card-Container">
+         {filteredServices.map((service) => (
+           <div
+             key={service.serviceName}
+             onClick={() => {
+               navigate(service.pageLink);
+             }}
+           >
+             
+             <Service serviceName={service.serviceName} notification={service.notification}   Icon={service.icon} />
+           </div>
+         ))}
+        
+       </div>
       </div>
-     <div className="card-Container">
-       {filteredServices.map((service) => (
-         <div
-           key={service.serviceName}
-           onClick={() => {
-             navigate(service.pageLink);
-           }}
-         >
-           
-           <Service serviceName={service.serviceName} notification={service.notification}   Icon={service.icon} />
-         </div>
-       ))}
-      
-     </div>
-    </div>
-  
-
+     
   );
 }
+
+export default TsvHomePage;
+  
+  
