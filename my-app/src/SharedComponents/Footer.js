e6330974fd656f98d15283b4acb654b2a0fd8f22
abdullahsaad2000 <harthi1127@gmail.com>
2024-03-03 15:22:41 +0300

    create a footer

---
+++ my-app/src/SharedComponents/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../SharedCSS/Footer.css";
+
+function Footer() {
+  return (
+    
+      <footer>
+        <p>&copy; All Rights reserved</p>
+      </footer>
+    
+  )
+}
+
+export default Footer
